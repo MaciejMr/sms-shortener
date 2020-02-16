edd8bf46c484914419d6dbb23554820ca7407bca
Łukasz Kamiński <lkamins7@gmail.com>
2020-02-16 12:54:39 +0000

    Switched junit dependency

--- src/test/java/pl/sdaacademy/sms/SmsShortenerTest.java
+++ src/test/java/pl/sdaacademy/sms/SmsShortenerTest.java
@@ -2,7 +2,8 @@ package pl.sdaacademy.sms;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class SmsShortenerTest {
 
